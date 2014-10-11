c60f770717b51fee2bac3792284bcd0a9e7b429b
Dan Scarafoni <dan@scarafoni.com>
2014-10-10 23:54:14 -0400

    working id read

--- index.py
+++ index.py
@@ -1,10 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 from redis import Redis
 
-
 # Initialize the Flask application
 app = Flask(__name__)
-redis = Redis()
 
 
 @app.route('/')
@@ -14,12 +12,12 @@ def index():
 
 @app.route('/get_id')
 def get_id():
-    f = open('total_players.txt', 'r')
+    f = open('data/total_players.txt', 'r')
     x = int(''.join(f.read().split()))
     x += 1
     print(x)
     f.close()
-    f = open('total_players.txt', 'w')
+    f = open('/datatotal_players.txt', 'w')
     f.write(str(x))
     f.close()
     return jsonify(result=x)
@@ -29,7 +27,7 @@ def get_id():
 def get_input():
     u_instruct = request.args.get('u_instruct', 0)
     # writ/e the result to the instructions
-    with open("instructions.txt", 'a') as f:
+    with open("data/instructions.txt", 'a') as f:
         f.write(str(u_instruct) + ". " + u_instruct + "\n")
 
     return jsonify(result="recieve input "+u_instruct+" thank you!")
@@ -42,14 +40,14 @@ def propose_instruct():
 
 @app.route('/vote_input')
 def send_choices():
-    with open("choices.txt", 'r') as f:
+    with open("data/choices.txt", 'r') as f:
         x = f.read()
         return jsonify(result=x.strip())
 
 
 @app.route('/get_instructions')
 def get_instructions():
-    with open("instructions.txt", 'r') as f:
+    with open("data/instructions.txt", 'r') as f:
         x = f.read()
         return jsonify(result=x)
 
@@ -57,12 +55,14 @@ def get_instructions():
 if __name__ == '__main__':
     # initialize everything
     with open('instructions.txt', 'w') as instructions,\
-        open('total_players.txt', 'w') as total_players,\
-        open('choices.txt', 'w') as choices,\
-            open('propositions.txt', 'w') as propositions:
+         open('total_players.txt', 'w') as total_players,\
+         open('choices.txt', 'w') as choices,\
+         open('propositions.txt', 'w') as propositions\
+         open('leader.txt', 'w') as leader:
             total_players.write('0')
             propositions.write('')
             instructions.write('')
+            leader.write('0')
             choices.write('')
 
     app.debug = True
