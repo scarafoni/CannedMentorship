1306309f727c308e3a26f944ea1dd9d080066b13
Dan Scarafoni <dan@scarafoni.com>
2014-10-20 18:47:11 -0400

    working draft of the clustering

--- ctest.py
+++ ctest.py
@@ -1,13 +1,15 @@
-from scipy.cluster.hierarchy import fclusterdata
+from scipy.cluster.hierarchy import fclusterdata, fcluster, linkage
 from numpy import ndarray
 
 data = [[1,1,1],\
         [10,10,10],\
         [1.5,1.5,1.5],\
-        [4,4,4],\
-        [9,9,9]]
+        [4,4,4]]
 
 x = fclusterdata(data,0.5)
 
+distances = [9,0.5,3,8.5,6,2.5]
+y = linkage(distances)
 print(x)
+print(fcluster(y,.9))
 
--- sort_answers.py
+++ sort_answers.py
@@ -1,16 +1,20 @@
-'''
-meant to group similar answers received from the website
-'''
-import mlpy
-
+from numpy import ndarray
+from scipy.cluster.hierarchy import fclusterdata, fcluster, linkage
+import nltk
 # transforms sentences into bag of words representation
-def bow(sentences):
+def to_bow_mat(sentences):
+    # filler
+    return ndarray(shape=(len(sentences),4))
+
 
 # hierarchical agglomerative classification algorithm
-def HAC(atures,dist_func):
-    x
-
-
+def hac(f_mat, dist_func='holder', thresh=0.5):
+    distances = []
+    if dist_func == 'holder':
+        return fclusterdata(f_mat,thresh)
+    else:
+        distances = dist_func(f_mat)
+    return fcluster(linkage(distances,thresh)) 
 
 if __name__== '__main__':
     inputs1 = [\
@@ -20,8 +24,8 @@ if __name__== '__main__':
                'get two slices of bread',\
                'get a knife'\
                ]
-    # f_mat1 = bow(inputs1)
-    # groups = hac(f_mat1)
+    f_mat1 = to_bow_mat(inputs1)
+    groups = hac(f_mat=f_mat1)
     print(groups)
     
               
