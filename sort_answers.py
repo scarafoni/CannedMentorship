52c78692aa69ca26a7fc2c9396649a018cdd1b73
Dan Scarafoni <dan@scarafoni.com>
2014-10-27 12:26:43 -0400

    working tfdif model

--- sort_answers.py
+++ sort_answers.py
@@ -6,16 +6,17 @@ from nltk.stem.porter import PorterStemmer
 import string
 
 
-# tokenization method from duke university
+# proprocessing inspired by duke university
 def stem_tokens(tokens, stemmer):
-    stemmer = PorterStemmer()
     stemmed = []
     for item in tokens:
         stemmed.append(stemmer.stem(item))
     return stemmed
 
+#tokenize and stem the text
 def tokenize(text):
     tokens = nltk.word_tokenize(text)
+    stemmer = PorterStemmer()
     stems = stem_tokens(tokens, stemmer)
     return stems
 
@@ -25,41 +26,43 @@ def tokenize(text):
 def preprocess(inputs, extraction_method='tfidf'):
     token_dict = {}
     # lower case sentences, remove punctuation
-    for sentence in sentences:
-        lowers = sentence.lower()
+    i = 1
+    for input in inputs:
+        lowers = input.lower()
         no_punctuation = lowers.translate(None, string.punctuation)
-        token_dict[file] = no_punctuation
+        token_dict['input '+str(i)] = no_punctuation
+        i += 1
 
-    # tokenize
-    tfidf = TfidfVectorizer(tokenizer=tokenize, stop_words='english')
 
     #create the feature matrix (tf-idf
     if extraction_method == 'tfidf':
+        # tokenize
+        tfidf = TfidfVectorizer(tokenizer=tokenize, stop_words='english')
         return tfidf.fit_transform(token_dict.values())
 
     return "error"
 
 
 # hierarchical agglomerative classification algorithm
-def hac(f_mat, dist_func='holder', thresh=0.5):
+def hac(f_mat, dist_func='custom', thresh=0.5):
     distances = []
-    if dist_func == 'holder':
-        return fclusterdata(f_mat,thresh)
+    if dist_func == 'custom':
+        return fclusterdata(X=f_mat.toarray(),t=thresh)
     else:
         distances = dist_func(f_mat)
     return fcluster(linkage(distances,thresh)) 
 
 if __name__== '__main__':
     inputs1 = [\
-               'spread the peanut butter',\
-               'spread the peanut butter with the knife',\
-               'spread the peanut butter on the bread',\
-               'get two slices of bread',\
-               'get a knife'\
+               'Spread the peanut butter.',\
+               'spread the Peanut butter with the knife.',\
+               'spread the Peanut butter on the bread.',\
+               'get two Slices. of bread.',\
+               'get a knife.'\
                ]
-    f_mat1 = to_bow_mat(inputs1)
-    print(f_mat1)
+    f_mat1 = preprocess(inputs1)
+    print(f_mat1.toarray())
     groups = hac(f_mat=f_mat1)
-    # print(groups)
+    print(groups)
     
               
