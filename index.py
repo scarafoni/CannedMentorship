917f901ef86da96eb4923ff18335b876388f84f5
Dan Scarafoni <dan@scarafoni.com>
2014-11-04 14:15:02 -0500

    commas

--- index.py
+++ index.py
@@ -262,8 +262,8 @@ def send_updates():
 
     #write state
     elif state == "write":
-        return jsonify(inputs=str(len(redis.lrange('inputs',0,-1)))\
-                   total_players=redis.get('total_players')\
+        return jsonify(inputs=str(len(redis.lrange('inputs',0,-1))),\
+                   total_players=redis.get('total_players'),\
                    instructions=instructions,\
                    leader=leader,\
                    state=state)
@@ -272,8 +272,8 @@ def send_updates():
     elif state == "vote":
         # vote_list = '\n'.join(redis.lrange('choices',0,-1))
         vote_list = (redis.lrange('choices',0,-1))
-        return jsonify(inputs=str(len(redis.lrange('inputs',0,-1)))\
-                       total_players=redis.get('total_players')\
+        return jsonify(inputs=str(len(redis.lrange('inputs',0,-1))),\
+                       total_players=redis.get('total_players'),\
                        instructions=instructions,\
                        choices=vote_list,\
                        leader=leader,\
@@ -287,8 +287,8 @@ def send_updates():
         
 
     elif state == 'vote_finish':
-        return jsonify(inputs=str(len(redis.lrange('inputs',0,-1)))\
-                       total_players=redis.get('total_players')\
+        return jsonify(inputs=str(len(redis.lrange('inputs',0,-1))),\
+                       total_players=redis.get('total_players'),\
                        instructions=instructions,\
                        leader=leader,\
                        state=state)
