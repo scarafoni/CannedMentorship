29bb794afc794bebc22fe0fbf2eac070cc89af98
Dan Scarafoni <dan@scarafoni.com>
2014-11-09 16:29:01 -0500

    form for users

--- filtered_obs2readable.py
+++ filtered_obs2readable.py
@@ -1,18 +1,25 @@
 with open('filtered_votes.txt','r') as r, open('readable_filtered.txt','w') as w:
     votes = r.read()
-    votes = votes.split('\n\n\n###\n')
+    votes = votes.split('\n###\n')
     # line split
     votes = [x.split('\n') for x in votes]
     #change the first line
     for i in range(len(votes)):
-        first_line = votes[i][0].split(',')
-        # remove the time, replace with 
-        first_line[-1] = 'Rating: _______'
-        # add the algorithm number
-        first_line.insert('Algorithm '+str(i))
-        # join all the arlgotithms by newline
-        first_line = ', '.join(first_line)
-        votes[i] = '\n'.join(votes)
+        k =1
+        for j in range(len(votes[i])):
+            if len(votes[i][j].split(',')) > 2:
+                first_line = votes[i][j].split(',')
+                # remove the time, replace with 
+                first_line[-1] = 'Rating: _______'
+                # add the algorithm number
+                first_line.insert(0, 'Algorithm '+str(k))
+                k += 1
+                # join all the arlgotithms by newline
+                first_line = ', '.join(first_line)
+                votes[i][j] = first_line
+        votes[i] = '\n'.join(votes[i])
+        w.write(votes[i])
+    
     
 
       
